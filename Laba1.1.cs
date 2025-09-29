a1ac6150f7209757ca43bbd5fbabcd1357eecd44
2yara <80318377+2Yara@users.noreply.github.com>
2025-09-29 21:50:14 +0900

    edit2

--- Laba1.1.cs
+++ Laba1.1.cs
@@ -3,14 +3,18 @@ using System;
 class Programm {
     static void Main() {
         Console.WriteLine("Введите х в градусах: ");
-        double x = Convert.ToDouble(Console.ReadLine());
+        double xDegrees = Convert.ToDouble(Console.ReadLine());
         double xRadians = xDegrees * Math.PI / 180;
 
         Console.WriteLine("Введите у в градусах: ");
-        double y = Convert.ToDouble(Console.ReadLine());
+        double yDegrees = Convert.ToDouble(Console.ReadLine());
         double yRadians = yDegrees * Math.PI / 180;
 
-        double z1 = Math.Cos(Math.Pow(xRadians, 4)) + Math.Sin(Math.Pow(yRadians, 2)) + (Math.Sin(Math.Pow(2*xRadians, 2))/4) - 1;
+        double cos4 = Math.Pow(Math.Cos(xRadians), 4);
+        double sin2y = Math.Pow(Math.Sin(yRadians), 2);
+        double sin22x = Math.Pow(Math.Sin(2 * xRadians), 2);
+
+        double z1 = cos4 + sin2y + (sin22x/4) - 1;
 
         double z2 = Math.Sin(yRadians + xRadians) * Math.Sin(yRadians - xRadians);
         
