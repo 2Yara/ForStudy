c754d933998eec2c248a57e5dc203fb3771e03ae
2yara <80318377+2Yara@users.noreply.github.com>
2025-09-30 10:17:22 +0900

    Update1

--- Laba2/Laba2.2.cs
+++ Laba2/Laba2.2.cs
@@ -4,22 +4,48 @@ class Programm
 {
   static void Main() 
   {
-    Console.WriteLine("Enter x: ");
+  Console.WriteLine("Enter x: ");
 
     double x = Convert.toDouble(Console.ReadLine());
 
     if ((-10 <= x) && (x <= -6)) {
+      double R = 2;
+      double y = Math.Sqrt(R*R - x*x) - 2;
+      Console.WriteLine($"if x = {x:F6}");
+      Console.WriteLine($"then y = {y:F6}");
       return;
     }
-    else if ((-6 <= x) && (x < 2)) {
+    else if ((-6 < x) && (x <= 2)) {
+      double y = 1 + x/2;
+      Console.WriteLine($"if x = {x:F6}");
+      Console.WriteLine($"then y = {y:F6}");
+      return;
+    }
+    else if ((2 < x) && (x < 6)) {
+      Console.WriteLine("f(x) is not defined");
       return;
     }
     else if ((6 <= x) && (x <= 8)) {
+      double y = Math.Pow((x - 6), 2);
+      Console.WriteLine($"if x = {x:F6}");
+      Console.WriteLine($"then y = {y:F6}");
       return;
     }
     else {
-      Console.WriteLine("");
+      Console.WriteLine("f(x) is not defined");
       return;
-    } 
+    }     
   }
 }
+
+// func Lab22() {}
+
+// Console.WriteLine("Enter lab num: ");
+//     char labNum = (char)Console.ReadLine();
+//     bool 
+//     switch(labNum) {
+//       case '2' : func Lab22 break;
+//       case '3' : func Lab32 break;
+//         default : 
+//     }
+    
