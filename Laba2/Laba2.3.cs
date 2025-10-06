841e4281823de5018117027560edbb7ea5d67e0e
2yara <80318377+2Yara@users.noreply.github.com>
2025-10-06 14:39:57 +0900

    Finale Laba2.3.cs

--- Laba2/Laba2.3.cs
+++ Laba2/Laba2.3.cs
@@ -2,32 +2,35 @@ using System;
 
 class Program {
   static void Main() {
-    Console.WriteLine("Введите радиус: ");
+    Console.WriteLine("Insert radius: ");
 //    double R = Convert.toDouble(Console.ReadLine());
     double R = 5;
-    Console.WriteLine("Введите координату x: ");
+    Console.WriteLine("Insert coordinate x: ");
 //    double x = Convert.toDouble(Console.ReadLine());
-    double x = 1;
-    Console.WriteLine("Введите координату y: ");
+    double x = -4;
+    Console.WriteLine("Insert coordinate y: ");
     //double y1 = Convert.toDouble(Console.ReadLine());
-    double y1 = 1;
+    double y1 = -1;
     double y;
     if (x >= -R && x <= 0) {
-      y = -x; 
-      double y2 = x;
-      if (y1 >= 0 && y1 <= y) {
-        Console.WriteLine($$"Координата [{x:F6},{y1:F6}] входит в область");
+      y = x; 
+      double y2 = -x;
+      if ((y1 > 0) && (y1 <= R && y1 >= y)) {
+        Console.WriteLine($"Coordinate [{x:F2},{y1:F2}] входит в область");
         return;
       }
-      if (y1 <= 0 && y1 >= y2) {
-        Console.WriteLine($$"Координата [{x:F6},{y1:F6}] входит в область");
+      else if ((y1 < 0) && (y1 >= -R && y1 <= y)) {
+        Console.WriteLine($"Coordinate [{x:F2},{y1:F2}] входит в область");
         return;
+      }
+      else {
+          Console.WriteLine("Координата не входит в область");
       }
     }
     else if ((x >= 0) && (x <= R)) {
       y = Math.Sqrt(R*R - x*x);
       if ((y >= y1) && (-y <= y1)) {
-        Console.WriteLine($$"Координата [{x:F6},{y1:F6}] входит в область");
+        Console.WriteLine($"Coordinate [{x:F2},{y1:F2}] входит в область");
         return;
       }
     }
