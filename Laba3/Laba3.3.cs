0166cc72ea0aefe2da510643d1862e7cd4c31867
2yara <80318377+2Yara@users.noreply.github.com>
2025-10-14 11:09:42 +0900

    Create Laba3.3.cs

---
+++ Laba3/Laba3.3.cs
@@ -0,0 +1,17 @@
+using System;
+
+class Program {
+    static void Taylors_Row(double x, double res) {
+      res = arctg((Math.Pi / 2) + )
+    }
+
+    static void Main() {
+        double x, res;
+        Console.WriteLine($"Taylors Row:");
+        Console.WriteLine("=====================================");
+        for (int n = 0; n < 5; n++) {
+            Taylors_Row(n, x, res);
+        }
+        Console.WriteLine("=====================================");
+    }
+}
