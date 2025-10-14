80db87826c0e52c1873ca35e5f229556bfa2226a
2yara <80318377+2Yara@users.noreply.github.com>
2025-10-14 10:57:24 +0900

    finale Laba3.2.cs

--- Laba3/Laba3.2.cs
+++ Laba3/Laba3.2.cs
@@ -1,36 +1,42 @@
 using System;
 
-class Program {
-  double R = 5;
-  static void Mishen(double Xshot, double Yshot) {
-    if (Xshot >= -R && Xshot <= 0) {
-      if ((Yshot > 0) && (Yshot <= R && Yshot >= y)) {
-        Console.WriteLine($"Bullet [{Xshot:F2},{Yshot:F2}] hit the target");
-      } else if ((Yshot < 0) && (Yshot >= -R && Yshot <= y)) {
-        Console.WriteLine($"Bullet [{Xshot:F2},{Yshot:F2}] hit the target");
-      } else {
-          Console.WriteLine("Bullet [{Xshot:F2},{Yshot:F2}] DID NOT hit the target");
-      }
-    } else if ((Xshot >= 0) && (Xshot <= R)) {
-      if ((R >= Yshot) && (-R <= Yshot)) {
-        Console.WriteLine($"Bullet [{Xshot:F2},{Yshot:F2}] hit the target");
-      }
-    } else {
-      Console.WriteLine("Bullet [{Xshot:F2},{Yshot:F2}] DID NOT hit the target");
+class Program 
+{
+    static double R = 5; 
+    
+    static void Mishen(double Xshot, double Yshot) 
+    {
+        if (Xshot >= -R && Xshot <= 0) {
+            if (Yshot > 0 && Yshot <= R && Yshot >= -Xshot) {
+                Console.WriteLine($"Bullet [{Xshot:F2},{Yshot:F2}] hit the target");
+            } else if (Yshot < 0 && Yshot >= -R && Yshot <= Xshot) {
+                Console.WriteLine($"Bullet [{Xshot:F2},{Yshot:F2}] hit the target");
+            } else {
+                Console.WriteLine($"Bullet [{Xshot:F2},{Yshot:F2}] DID NOT hit the target");
+            }
+        } 
+        else if (Xshot >= 0 && Xshot <= R) {
+            if (Xshot * Xshot + Yshot * Yshot <= R * R) {
+                Console.WriteLine($"Bullet [{Xshot:F2},{Yshot:F2}] hit the target");
+            } else {
+                Console.WriteLine($"Bullet [{Xshot:F2},{Yshot:F2}] DID NOT hit the target");
+            }
+        } else {
+            Console.WriteLine($"Bullet [{Xshot:F2},{Yshot:F2}] DID NOT hit the target");
+        }
     }
-  }
 
-  
-  static void Main() {
+    static void Main() {
         Random rand = new Random();
         
+        Console.WriteLine($"Trying target with radius R = {R:F2}");
+        Console.WriteLine("=====================================");
+        
         for (int i = 0; i < 10; i++) {
-            double Xshot = rand.NextDouble(-6, 7); // от -6 до 6
-            double Yshot = rand.NextDouble(-6, 7); // от -6 до 6
+            double Xshot = (rand.NextDouble() * 13) - 6; // от -6 до 6
+            double Yshot = (rand.NextDouble() * 13) - 6; // от -6 до 6
             
             Mishen(Xshot, Yshot);
         }
-    return;
     }
 }
-
