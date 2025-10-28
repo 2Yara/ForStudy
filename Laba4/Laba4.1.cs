7096ac033c89449f5da94bb286931658977410e7
2yara <80318377+2Yara@users.noreply.github.com>
2025-10-28 11:45:05 +0900

    Update Laba4.1.cs

--- Laba4/Laba4.1.cs
+++ Laba4/Laba4.1.cs
@@ -1,8 +1,32 @@
 using System;
 
 class Program {
-  class Data {
-    
+  public class Data {
+    private int year;
+    private int month;
+    private int day;
+
+    private const int MinYear = 1;
+    private const int MaxYear = 9999;
+    private readonly int[] dayinMonth = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+    public Data() {
+      DateTime today = DateTime.Today;
+      year = today.Year;
+      month = today.Month;
+      day = today.Day;
+    }
+
+    public Data(int year, int month, int day) {
+      SetDate(year, month, day);
+    }
+
+    public int Year {
+      get {return year;}
+      set {
+        if ((value < MinYear)||(value > MaxYear)) throw new 
+      }
+    }
   }
 
   
